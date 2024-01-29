a7d0e174ccc2c886fbbebbb73e1204da102089b6
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-01-29 21:10:04 +0000

    addedLink

--- my-app/src/pages/Projectlist.js
+++ my-app/src/pages/Projectlist.js
@@ -1,5 +1,5 @@
 import Tortee from "./tortee.png"
-import weather from "./weather.png"
+import spiral from "./Spiral.png"
 import { DiGithubBadge } from "react-icons/di";
 import { RiPresentationLine } from "react-icons/ri";
 import { TbBrandVercel } from "react-icons/tb";
@@ -34,39 +34,37 @@ export const ProjectList = [
     
     desc:  (
       <>
-        • Project LendLogic is a testament to the transformative power of technology, where I played a pivotal role in its development.<br /><br />
-        • Employing a sophisticated tech stack encompassing JavaScript, CSS, Next.js, Vercel, Git, and a SQL database, the app embodies a harmonious blend of functionality and aesthetics.
+        • Project LendLogic reflects my pivotal role in solving real-world problems for a prominent company, emphasizing user experience and a streamlined journey when regarding their mortgage choices.<br /><br />
+        •  Employing a cutting-edge tech stack comprising React, TypeScript, Tailwind CSS, Firebase, and Git, LendLogic epitomizes a seamless fusion of user-centric design and robust functionality.
         <br /><br />
-        • Leveraging my expertise, I implemented CRUD features on a robust API, ensuring seamless communication and interaction between mentors and mentees.
-        <br /><br /> •The application not only boasts an elegant design but also incorporates my proficiency in data management, API integration, and version control through Git.
+        Drawing upon my expertise, I implemented a streamlined user journey, focusing on intuitive interactions and a visually pleasing interface. Through meticulous attention to user experience, LendLogic ensures that every user, whether a lender or borrower, navigates the platform effortlessly.
+        <br /><br /> • From crafting personalized dashboards tailored to individual users to implementing individual tools to help our users on their mortgage journey, each feature in LendLogic is a result of careful consideration, aiming to enhance the user experience and journey. The platform transcends conventional lending practices, fostering a dynamic space for seamless collaboration and mutual empowerment within the financial landscape.
         <br /><br />
         • Additionally, my skills in optimizing performance and ensuring scalability have contributed to an app that provides a user-friendly and responsive experience.
-        <br /><br /> • From creating personalized dashboards to implementing real-time notifications, every feature has been meticulously developed to enrich the mentorship journey. With this app, I have not only showcased my technical prowess but have also actively contributed to building a platform that transcends traditional mentorship boundaries, fostering a dynamic space for growth, collaboration, and mutual empowerment.
+        <br /><br />
       </>
   
     ),
     speech:'"I think what I learnt most from this project was the working with actual stakeholders. We tackled a problem given to us by a real company. So it was crucial there and our teams visons were cohesive. I learnt It was important to set realstic expectations and make sure to keep open honest communcation throughout"',
   link: <a href="https://github.com/MartiZu/LendLogic-app" target="_blank" rel="noreferrer" ><DiGithubBadge /></a>,
-  demo: <a href="https://www.youtube.com/watch?v=iuWxCgkibsM&feature=youtu.be" target="_blank" rel="noreferrer" ><RiPresentationLine /> </a>,
+  demo: <a href="https://www.youtube.com/watch?v=b2MLi2V68e4" target="_blank" rel="noreferrer" ><RiPresentationLine /> </a>,
   deployed: <a href="https://land-logic-app-frontend.vercel.app/" target="_blank" rel="noreferrer" ><TbBrandVercel /> </a>,
 }
- ,{ name: "Project Under The Weather",
-image: weather,
+ ,{ name: "Project Spiral",
+image: spiral,
  desc:  (
         <>
           <br></br>•
-"Let me share the journey of creating 'Under the Weather' , my first exploration into APIs and DOM manipulation. <br>
+"Let me share the journey of creating 'Spiral' , a mental health app aimed at stopping negative thoughts in their tracks. <br>
 </br> <br>
-</br> • This app was my hands-on lesson on learning about APIs and dom manipulation.<br>
+</br> • Developed using cutting-edge technologies, the app is built with Next.js and utilizes the sleek design capabilities of Tailwind CSS for a user-friendly experience. <br>
 </br> <br>
-</br> • Under the Weather' taught me to use the Fetch API, pulling real-time temperature data for any location. Learning asynchronous operations made the app stay lively and responsive while fetching data. <br></br>
+</br> • Spiral's primary goal is to intercept and interrupt negative thought patterns, preventing them from spiraling out of control. The app achieves this through a combination of personalized advice based on keywords of the users input.  <br></br>
 <br></br>
-• Then, DOM manipulation brought a visual twist. Clicks and inputs triggered instant updates, teaching me all about the importance of user interaction.
+
 <br>
 </br><br></br>
-
-•Under the Weather' isn't just a weather app; it's my coding adventure. With every line of code, I discovered the thrill of bringing data to life and creating a personal user experience.
-<br></br> <br></br>• This app is not just about weather updates; it's about embracing the excitement of learning and coding. Your weather updates, my way  all thanks to 'Under the Weather.'
+<br></br> <br></br>
         <br></br>
         <br></br>
         <br></br>
@@ -76,5 +74,5 @@ image: weather,
       ),
       speech:'"I think what I learnt most from this project was the unpredictabitly of using free public APIS. You pay for what you get and when its free it can be sometiems unreliable. However it was a good way of learning how to incoperate and fetch the information you needed from those kind of APIS "',
     link: <a href="https://github.com/SchoolOfCode/bc15-w3-d5-WEATHER-API" target="_blank" rel="noreferrer" ><DiGithubBadge /></a>,
-    demo: <a href="https://github.com/SchoolOfCode/Team-expedition" target="_blank" rel="noreferrer" ><RiPresentationLine /> </a>,
-    deployed: <a href="https://github.com/SchoolOfCode/Team-expedition" target="_blank" rel="noreferrer" ><TbBrandVercel /> </a>, }]
+    demo: <a href="https://github.com/joeodx/Spiral" target="_blank" rel="noreferrer" ><RiPresentationLine /> </a>,
+    deployed: <a href="https://spiral-mental-health.vercel.app/" target="_blank" rel="noreferrer" ><TbBrandVercel /> </a>, }]
